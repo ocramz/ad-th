2f96abb46e2a31152bfb7d7686fcd9baf9332a4e
Marco Zocca <marco.zocca@unfoldml.com>
2021-06-18 10:59:40 +0200

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# ad-th
+
+[![Build Status](https://travis-ci.org/ocramz/ad-th.png)](https://travis-ci.org/ocramz/ad-th)
+
+TODO Description.
---
+++ src/Numeric/AD/TH.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Numeric.AD.TH where
+
+-- haskell-src-meta
+import Language.Haskell.Meta.Parse (parseDecs)
+-- template-hasekll
+import Langauge.Haskell.TH.Quote (QuasiQuoter(..))
